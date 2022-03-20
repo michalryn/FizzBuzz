4c607659f4b0e470be6b7d2ab4da6abd35ffd3f6
Michał Ryniewicz <misiekxyz18@gmail.com>
2022-03-21 00:43:19 +0100

    Dodaj pliki projektów.

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb2.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"),
+         Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int? Number { get; set; }
+        
+        /*public string? Check()
+        {
+            if (Number % 3 == 0 && Number % 5 == 0)
+                return "FizzBuzz";
+            else if (Number % 3 == 0)
+                return "Fizz";
+            else if (Number % 5 == 0)
+                return "Buzz";
+            else if (Number == null)
+                return null;
+            return $"Liczba: {Number} nie spełnia kryteriów FizzBuzz";
+        }*/
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb2.Models;
+
+namespace FizzBuzzWeb2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                if (string.IsNullOrWhiteSpace(Name))
+                {
+                    Name = "User";
+                }
+                return Page();
+            }
+            /*
+            TempData["AlertMessage"] = FizzBuzz.Check();
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }*/
+            return Page();
+        }
+
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+    }
+}
